cb068ff5dfcd0b648b2a35d058fea7940b19231a
loren <l.carpentieri7@studenti.unisa.it>
2021-06-17 09:50:25 +0200

    Character models

--- models/character.js
+++ models/character.js
@@ -5,10 +5,11 @@ class Character {
         this.name = character.name
         this.scenes = character.scenes
     }
+
     
     /* 
         Input: nome di un personaggio
-
+        Output: lista dei film in cui il personaggio appare
     */
     static async filmsInWhichCharacterAppeared(characterName) {
         if(!characterName){
@@ -17,7 +18,7 @@ class Character {
         
         const session = driver.session();
         return session.run('MATCH (character:Character {name: $name})-[r:APPEARED_IN]->(film:Film)' +
-                    'RETURN film',
+                           'RETURN film',
                     {
                         name: characterName
                     }
@@ -28,26 +29,75 @@ class Character {
                 })
     }
 
+    /*
+        Input: lista nomi personaggio (list) e titolo di un film (f)
+        Output: lista delle interazioni dei personaggi nella lista 'list' nel film 'f' 
+    */
     static async characterInteractionsInFilm(characterNames, filmName) {
         if(!characterNames)
             throw new Error('No characterNames parameter')
 
         if(!filmName)
             throw new Error('No filmName parameter')
+       
         
-        session.run('MATCH (character:Character)-[r:SPEAK_WITHIN_IN_THE_SAME_SCENE{film: $film}]-(c)' +
-                    'WHERE character.name in $characters' +
+        const session = driver.session()
+        
+        return session.run('MATCH (character:Character)-[r:SPEAK_WITHIN_IN_THE_SAME_SCENE{film:$film}]-(c) ' +
+                    'WHERE character.name in $characters ' +
                     'RETURN character, r, c',
                     {
                         characters: characterNames,
                         film: filmName
                     })
                     .then((result)=>{
-                        console.log(result)
+                        const c1 = result.records.map((r)=>r.get('character').properties)
+                        const r = result.records.map((r)=>r.get('r').properties)
+                        const c2 = result.records.map((r)=>r.get('c').properties)
+                        
+                        const zip = (a, b, c) => a.map((k, i) => [k, b[i], c[i]])
+
+                        
+                        const speakWith = zip(c1,r,c2)
+                        session.close()
+                        return speakWith
                     })
+
     }   
 
+    /*
+        Input: 2 nomi
+        Output: film in cui i due personaggi compaiono insieme e numero di volte in cui accade
+    */
+    static async timesTwoCharactersAppearToghether(characterName1, characterName2){
+        if(!characterName1)
+            throw new Error('No characterName1 parameter')
+        
+        if(!characterName2)
+            throw new Error('No characterName2 parameter')
+        
+        const session = driver.session()
+        
+        return session.run('MATCH (c1:Character)-[r:SPEAK_WITHIN_IN_THE_SAME_SCENE]-(c2:Character) '+
+                    'WHERE c1.name = $name1 AND c2.name = $name2 '+
+                    'RETURN c1, collect(r.film), c2, sum(r.times)',{
+                        name1: characterName1,
+                        name2: characterName2
+                    })
+                    .then((result)=>{
+                        const films = result.records.map(r => r.get('collect(r.film)'))
+                        const sum = result.records.map(r => r.get('sum(r.times)'))
+                        
+                        const zip = (a, b) => a.map((k, i) => [k, b[i]])
+                        const timeSpeaks = zip(films, sum)
+                        console.log(timeSpeaks)
+                        session.close()
 
+                        return timeSpeaks
+                    })
+
+    }
+    
 }
 
 
--- services/characterService.js
+++ services/characterService.js
@@ -1,13 +1,25 @@
 import characterControl from '../controllers/characterController.js';
 
 export default (app) => {
+  /* 
+        Input: nome di un personaggio
+        Output: lista dei film in cui il personaggio appare
+  */
   app.get('/character/:characterName', characterControl.films);
   
+   /*
+        Input: lista nomi personaggio (list) e titolo di un film (f)
+        Output: lista delle interazioni dei personaggi nella lista 'list' nel film 'f' 
+    */
   app.get('/character/:characters/:filmTitle', function(req, res) {
       res.send('hello world');
     });
-
-  app.get('/character/speak/:characterNames', function(req, res) {
+  
+  /*
+      Input: 2 nomi
+      Output: film in cui i due personaggi compaiono insieme e numero di volte in cui accade
+  */
+  app.get('/character/speak/:characterName1/:characterName2', function(req, res) {
       res.send('hello world');
   });
 }
